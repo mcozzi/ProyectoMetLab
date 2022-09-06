9cf949239451cccb2b166d432bcbabac0dea9318
mcozzi <96800700+mcozzi@users.noreply.github.com>
2022-09-06 19:59:16 -0300

    fetch questions

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,23 @@
 import Button from "../../components/Button";
 import { useNavigate } from 'react-router-dom';
+import background from "../../assets/background.png"
 
 function Home() {
     const navigate = useNavigate();
 
-    const onClickButton = (url) => {
-        navigate(url);
-    }
+    const onClickButton = () => {
+        navigate('/game');
+     } 
 
-return(
-    <Button onClick={() => onClickButton('/game')} text='MetCamp Quiz' />
-)
-}
+     return (
+         <section className="hero is-medium is-fullheight" style={{backgroundImage: `url(${background})`, backgroundSize: "cover"}}>
+         <div className="hero-body">
+             <div className="container has-text-centered">
+                 <Button onClick={() => onClickButton('/game')} text='MetCamp Quiz' />
+             </div>
+         </div>
+     </section>
+     )
+    }
 
 export default Home;
